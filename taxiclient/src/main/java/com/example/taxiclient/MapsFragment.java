f4092f0dd2bff4ceb47073a79b10588686b8e1b3
mk <maksatbek036@gmail.com>
2025-12-14 20:59:21 +0300

    map 2

--- taxiclient/src/main/java/com/example/taxiclient/MapsFragment.java
+++ taxiclient/src/main/java/com/example/taxiclient/MapsFragment.java
@@ -1,24 +1,33 @@
 package com.example.taxiclient;
 
+import static com.google.android.gms.location.LocationServices.*;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.Manifest;
+import android.companion.DeviceId;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationRequest;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApi;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.LocationSource;
@@ -30,22 +39,22 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.Task;
 
-public class MapsFragment extends Fragment implements OnMapReadyCallback{
+import java.util.Objects;
 
-    private static final int LOCATION_PERMISSION_REQUEST_CODE =101 ;
+public class MapsFragment extends Fragment implements OnMapReadyCallback
+        ,GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        com.google.android.gms.location.LocationListener
+{
 
-    Location currentLocation;
-   FusedLocationProviderClient fusedLocationProviderClient;
+ GoogleMap map;
+GoogleApiClient client;
+Location lastLocation;
+LocationRequest locationRequest;
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng l=new LatLng(-50,150);
-googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-googleMap.moveCamera(CameraUpdateFactory.newLatLng(l));
-MarkerOptions options=new MarkerOptions().position(l).title("Ss");
-options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW));
 
         }
     };
@@ -74,6 +83,43 @@ options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_Y
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+map=googleMap;
+buildGoogleApiClient();
+map.setMyLocationEnabled(true);
+    }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        if(ActivityCompat.checkSelfPermission(Objects.requireNonNull(getContext()),Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
+
+        locationRequest= new LocationRequest.Builder(10000).build();
+
+
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+lastLocation=location;
+
+LatLng latLng=new LatLng(location.getLatitude(),location.getLongitude());
+map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+map.animateCamera(CameraUpdateFactory.zoomTo(12));
+    }
+    protected synchronized void buildGoogleApiClient(){
+        client=new GoogleApiClient.Builder(this.getContext()).addConnectionCallbacks(this).addOnConnectionFailedListener(this)
+                .addApi(API).build();
+        client.connect();
     }
 }
--- taxiclient/src/main/java/com/example/taxiclient/Point.java
+++ taxiclient/src/main/java/com/example/taxiclient/Point.java
@@ -1,20 +1,30 @@
 package com.example.taxiclient;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 /**
 ate an instance of this fragment.
  */
-public class Point extends Fragment {
+public class Point extends Fragment  {
 EditText editTextPointA;
 EditText editTextPointB;
+Button btn;
+
 
 
     @Override
@@ -24,15 +34,28 @@ EditText editTextPointB;
 
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View root=inflater.inflate(R.layout.fragment_point,container,false);
-editTextPointA=root.findViewById(R.id.edit_text_a);
-editTextPointB=root.findViewById(R.id.edit_text_b);
+
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_point, container, false);
 
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        editTextPointA=view.findViewById(R.id.edit_text_a);
+        editTextPointB=view.findViewById(R.id.edit_text_b);
+        btn=view.findViewById(R.id.btn_order);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), FormPay.class));
+            }
+        });
     }
 }
